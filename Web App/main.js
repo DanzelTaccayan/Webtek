5721dc76909983c1580c39811e5ca9979af0c6c8
Miguel Delos Santos <2153820@slu.edu.ph>
2017-03-29 23:35:58 +0800

    added generate table method by borrower and manipulate he value of local storage by subracting its value

--- Web App/main.js
+++ Web App/main.js
@@ -4,12 +4,22 @@ var borrowersArray = [];
 
 /** ADDING ITEMS **/
 //when the body of the html loads do this function
-document.body.onload = function() {
+function initItem() {
 	console.log("body loaded");
 	if(localStorage.itemsRecord){
 		itemsArray = JSON.parse(localStorage.itemsRecord);
 		for (var i = 0; i < itemsArray.length; i++) {
 			generateTableItems(itemsArray[i].Description,itemsArray[i].Quantity);
+		}
+	} 
+}
+
+function initBorrow() {
+	if (localStorage.loanRecord) {
+		borrowersArray = JSON.parse(localStorage.loanRecord);
+		for (var i = 0; i < borrowersArray.length; i++) {
+			generateTableBorrower(borrowersArray[i].Idnum, borrowersArray[i].Name, borrowersArray[i].Item, 
+				borrowersArray[i].Quantity, borrowersArray[i].Duedate, borrowersArray[i].DateBorrowed);
 		}
 	}
 }
@@ -68,20 +78,67 @@ function validate(){
 function addBorrower(){
     var id = document.getElementById("user_id").value;
     var name = document.getElementById("user_name").value;
-    var quantity = document.getElementById("input_quantity").value;
+    var quantityBorrow = document.getElementById("input_quantityBorrow").value;
     var item = document.getElementById("item_description").value;
     var date = document.getElementById("date_return").value;
     var dateBorrowed = new Date();
+    var tempArray = [];
+    tempArray = JSON.parse(localStorage.itemsRecord);
+ 
 
-    if (id === "" || name === "" || quantity === "" || isNaN(quantity) || date === "" || isNaN(id)) { 
+    if (id === "" || name === "" || quantityBorrow === "" || date === "" ) { 
         alert("Complete the information needed");
-    } else {
-        
+    } else if (isNaN(quantityBorrow) || isNaN(id)) {
+    	alert("Quantity and ID expecting number format");
+    } else {	
+    	for (var i = 0; i < tempArray.length; i++) {
+    		if (item === tempArray[i].Description) {
+    			if (tempArray[i].Quantity < quantityBorrow) {
+    				alert("The Quantity entered exceed to the Quantity available of a Item ");
+    			} else {
+    				tempArray[i].Quantity -= quantityBorrow;
+    				localStorage.itemsRecord = JSON.stringify(tempArray);
+			        var borrowersObj = {'Idnum': id, 'Name': name, 'Item': item, 'Quantity': quantityBorrow, 'DateBorrowed':dateBorrowed,'Duedate': date };
 
-        var borrowersObj = {'Idnum': id, 'Name': name, 'Item': item, 'Quantity': quantity, 'Date borrowed':dateBorrowed,'Duedate': date };
+			        borrowersArray.push(borrowersObj);
 
-        borrowersArray.push(borrowersObj);
-
-        localStorage.loanRecord =JSON.stringify(borrowersArray);  
+			        localStorage.loanRecord =JSON.stringify(borrowersArray);  
+			        generateTableBorrower(id, name, item, quantityBorrow, date, dateBorrowed);
+			        break;
+    			}
+    		}
+    	}		
     } 
+}
+
+function generateTableBorrower(id, name, item, quantityBorrow, date, dateBorrowed  ) {
+	//Query table
+	var table = document.querySelector("table[name=borrowerTable]");
+	//Insert a row into the table
+	var tabRow = table.insertRow();
+
+	//Insert into the first and second column of the tabRow var
+	var colStudentId = tabRow.insertCell(0);
+	var colStudentName = tabRow.insertCell(1);
+	var colItemName = tabRow.insertCell(2);
+	var colItemQuant = tabRow.insertCell(3);
+	var colDateBorrow = tabRow.insertCell(4);
+	var colDateReturn = tabRow.insertCell(5);
+	var colAction = tabRow.insertCell(6);
+
+	//put some values
+	colStudentId.textContent = id;
+	colStudentName.textContent = name;
+	colItemName.textContent = item;
+	colItemQuant.textContent = quantityBorrow;
+	colDateBorrow.textContent = dateBorrowed;
+	colDateReturn.textContent = date;
+	colAction.innerHTML = "<button> view details </button>"
+
+	//Clear the values of the input
+	document.getElementById("user_id").value = "";
+	document.getElementById("user_name").value = "";
+	document.getElementById("date_return").value = "";
+	document.getElementById("item_description").value = "";
+	document.getElementById("input_quantityBorrow").value = "";
 }
