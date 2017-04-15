6a521f9b72c4c320b3025f4ddb3cc2e2aa84d299
aj <aj.gmail.com>
2017-04-15 21:22:24 +0800

    Design for viewDetails(modal form) returner.html
    Design includes the pop up modal form for additional items on view
    details. adding design for returner.html

--- Web App/AddRequest/Webapp/design.js
+++ Web App/AddRequest/Webapp/design.js
@@ -2,15 +2,11 @@
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var modalBtn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-modalBtn.onclick = function() {
-    modal.style.display = "block";
-}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -22,4 +18,7 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+function display(){
+    modal.style.display = "block";
 }
